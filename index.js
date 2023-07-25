1c4812dbf262eca9f92f260a33773f13fa3af21b
jokay03J <magrojoan3@gmail.com>
2023-07-25 16:00:10 +0200

    update index.js
    * refactor checkWin
    * refactor cells
    * add colors in render

--- index.js
+++ index.js
@@ -1,72 +1,107 @@
 #!/usr/bin/env node
+import chalk from "chalk";
 import { createInterface } from "readline";
 
 class Game {
   constructor() {
-    this.cells = new Array(9).fill("_");
+    this.rows = 3;
+    this.columns = 3;
+    this.cells = this.generate(this.columns, this.rows);
     this.play = true;
     this.turn = "X";
     this.count = 0;
     this.matchResult = "";
   }
 
-  checkWin() {
-    const winPattern = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [6, 4, 2],
-    ];
-
-    for (
-      let indexPatterns = 0;
-      indexPatterns < winPattern.length;
-      indexPatterns++
-    ) {
-      const patterns = winPattern[indexPatterns];
-      let patternsIsValid = true;
-      for (
-        let indexPattern = 0;
-        indexPattern < patterns.length;
-        indexPattern++
-      ) {
-        const pattern = patterns[indexPattern];
-        if (this.cells[pattern] !== this.turn) {
-          patternsIsValid = false;
-        }
+  generate(col, row) {
+    const result = [];
+    for (let indexRow = 0; indexRow < row; indexRow++) {
+      const row = [];
+      for (let indexColumn = 0; indexColumn < col; indexColumn++) {
+        row.push("_");
       }
-      if (patternsIsValid) {
+      result.push(row);
+    }
+
+    return result;
+  }
+
+  checkWin(row, column) {
+    let count = 0;
+    //vertical
+    for (let indexRow = 0; indexRow < this.cells.length; indexRow++) {
+      count = this.cells[indexRow][column] === this.turn ? count + 1 : 0;
+      if (count >= this.rows) {
+        this.matchResult = this.turn;
+        this.play = false;
         return true;
       }
     }
 
+    //horizontal
+    for (
+      let indexColumn = 0;
+      indexColumn < this.cells[row].length;
+      indexColumn++
+    ) {
+      count = this.cells[row][indexColumn] === this.turn ? count + 1 : 0;
+      if (count >= this.rows) {
+        this.matchResult = this.turn;
+        this.play = false;
+        return true;
+      }
+    }
+
+    //diagonal
+    let indexRow = 0;
+    let indexColumn = 0;
+    while (indexRow < this.rows && indexColumn < this.columns) {
+      count = this.cells[indexRow][indexColumn] === this.turn ? count + 1 : 0;
+      if (count >= this.rows) {
+        this.matchResult = this.turn;
+        this.play = false;
+        return true;
+      }
+      indexColumn++;
+      indexRow++;
+    }
+
+    //anti-diagonal
+    indexRow = 0;
+    indexColumn = this.columns - 1;
+    while (indexRow < this.rows && indexColumn < this.columns) {
+      count = this.cells[indexRow][indexColumn] === this.turn ? count + 1 : 0;
+      if (count >= this.rows) {
+        this.matchResult = this.turn;
+        this.play = false;
+        return true;
+      }
+      indexColumn--;
+      indexRow++;
+    }
     return false;
   }
 
-  put(index) {
-    if (this.cells[index] !== "_") {
+  put(row, column) {
+    if (this.cells[row][column] !== "_") {
       throw new Error("Tu as déjà jouer ici !");
     }
-    this.cells[index] = this.turn;
-    const isWin = this.checkWin();
+    this.cells[row][column] = this.turn;
+    const isWin = this.checkWin(row, column);
     if (isWin) {
       this.play = false;
       this.matchResult = this.turn;
     }
     this.turn = this.turn === "X" ? "O" : "X";
     this.count++;
-    if (this.count >= this.cells.length) {
+    if (this.count >= this.rows * this.columns) {
       this.play = false;
       this.matchResult = "Raw";
     }
   }
 
   reset() {
-    this.cells = new Array(9).fill("_");
+    this.cells = this.generate(this.columns, this.rows);
     this.play = true;
     this.turn = "X";
     this.count = 0;
@@ -91,17 +126,16 @@ class Graphic {
   }
 
   renderTable(cells) {
-    let result = "|%||0||1||2|\n";
-    for (let index = 0; index < cells.length; index++) {
-      const cell = cells[index];
-      if (index % 3 === 0) {
-        result += `|${index / 3}||${cell}|`;
-      } else {
-        result += `|${cell}|`;
+    let result = `${chalk.blue("|%||0||1||2|")}\n`;
+    for (let indexRows = 0; indexRows < cells.length; indexRows++) {
+      const column = cells[indexRows];
+      for (let indexColumns = 0; indexColumns < column.length; indexColumns++) {
+        const cell = column[indexColumns];
+        result += `${
+          indexColumns === 0 ? chalk.cyan(`|${indexRows}|`) : ""
+        }|${cell}|`;
       }
-      if (index === 2 || index === 5 || index === 8) {
-        result += "\n";
-      }
+      result += "\n";
     }
     this.rl.write(result + "\n");
   }
@@ -122,33 +156,55 @@ function main() {
     render.write(`Partie terminer !\nResultat: ${game.matchResult}\n`);
     process.exit(0);
   }
+  let row = 0;
+  let column = 0;
   render.question(
-    `Vous devez choisir l'index de la case !\nC'est aux tour de ${game.turn}\n> `,
+    `C'est au tour de ${game.turn}\n${chalk.cyan("Ligne(0-2)")} > `,
     (answer) => {
       if (answer === "q") {
         process.exit(0);
       }
       if (answer === "reset") {
+        render.write("Le jeu à bien été réintialisé !\n");
         game.reset();
-        return main();
-      }
-
-      if (!answer.match(/[0-8]/gm)) {
-        render.write("Format incorrect !\n");
-        return main();
-      }
-
-      try {
-        Graphic.clear();
-        game.put(parseInt(answer));
-        render.renderTable(game.cells);
-      } catch (error) {
-        render.write(error.message + "\n");
         render.renderTable(game.cells);
         return main();
       }
 
-      main();
+      row = parseInt(answer, 10);
+      if (isNaN(row) || row > 2 || row < 0) {
+        render.write(chalk.red("Ligne incorrect ! (0-2)\n"));
+        return main();
+      }
+
+      render.question(`${chalk.blue("Colonne(0-2)")} > `, (answer) => {
+        if (answer === "q") {
+          process.exit(0);
+        }
+        if (answer === "reset") {
+          game.reset();
+          return main();
+        }
+
+        column = parseInt(answer, 10);
+        if (isNaN(column) || column > 2 || column < 0) {
+          render.write(chalk.red("Colonne incorrect ! (0-2)\n"));
+          return main();
+        }
+
+        try {
+          Graphic.clear();
+          game.put(row, column);
+          render.renderTable(game.cells);
+        } catch (error) {
+          render.write(error.message + "\n");
+          render.write(error.stack + "\n");
+          render.renderTable(game.cells);
+          return main();
+        }
+
+        main();
+      });
     }
   );
 }
